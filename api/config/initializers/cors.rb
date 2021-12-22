a2833b1915b8d4a89415c309dc9f6bc0f3e9a601
Jon Staples <jon@Jons-MacBook-Pro.local>
2021-12-22 11:05:20 -0500

    task: Get CORS working

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -6,7 +6,6 @@
 # Read more: https://github.com/cyu/rack-cors
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     # FIXME:Obviously insecure, optimizing for time/simplicity here not security
      origins "*"
      resource "*",
        headers: :any,

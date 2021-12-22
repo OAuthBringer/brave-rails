9b23b696b165a0c612492bc7a31e807326e6a9ba
Jon Staples <jon@Jons-MacBook-Pro.local>
2021-12-22 18:00:59 -0500

    improvement: Add refresh task to async update cache

--- api/app/services/swapi/cache.rb
+++ api/app/services/swapi/cache.rb
@@ -3,6 +3,10 @@ class Swapi::Cache
     data = $redis.get(url)
     return JSON.parse(data) if data
 
+    self.set(url)
+  end
+
+  def self.set(url)
     data = Swapi::Client.get(url) 
     $redis.set(url, data.to_json)
 

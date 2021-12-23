3c072d6d790e7f56629d4db465c86760dd8840af
Jon Staples <jon@Jons-MacBook-Pro.local>
2021-12-23 09:14:04 -0500

    task: Polish up specs and mock redis

--- api/spec/requests/base_spec.rb
+++ api/spec/requests/base_spec.rb
@@ -1,7 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe "Bases", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+# These specs are actually firing off real HTTP requests which is not
+# really advisible for various reasons (namely CI/CD) but for the moment 
+# I'm ignoring this
+
+RSpec.describe "Application", type: :request do
+  let(:url) { "https://swapi.dev/api" }
+  let(:params) { { url: url } }
+
+  describe "root" do
+    before do
+      get root_path
+    end
+
+    it "200" do
+      expect(response).to have_http_status 200
+    end
+  end
+
+  describe "swapi" do
+    before do
+      post swapi_path, params: params
+    end
+
+    it "200" do
+      expect(response).to have_http_status 200
+    end
   end
 end
